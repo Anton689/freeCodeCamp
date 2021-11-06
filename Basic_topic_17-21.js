08e8bf822e2ae9d8e7f7fc79f89f63bc0609eb65
Anton689 <zizugum@gmail.com>
2021-11-06 14:34:12 +0300

    basic JS 17-21 topics

---
+++ Basic_topic_17-21.js
@@ -0,0 +1,28 @@
+//topic 17
+const product = 2.0 * 2.5;
+console.log(product);
+
+//topic 18
+
+const quotient = 4.4 / 2.0; // Change this line
+
+//topic 20
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+//topic 21
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
+
