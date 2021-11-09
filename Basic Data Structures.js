a0df77c7ef49f117ceafa41586f90577b5faebb5
Anton689 <zizugum@gmail.com>
2021-11-09 17:57:45 +0300

    added new solutions

--- Basic Data Structures.js
+++ Basic Data Structures.js
@@ -12,3 +12,24 @@ myArray[1] = 'Hello';
 
 console.log(myArray);
 
+//Add Items to an Array with push() and unshift()
+
+function mixedNumbers(arr) {
+	arr.unshift("I", 2, "three");
+	arr.push(7, "VIII", 9);
+	return arr;
+ }
+ 
+ console.log(mixedNumbers(['IV', 5, 'six']));
+
+ //Remove Items from an Array with pop() and shift()
+
+ function popShift(arr) {
+	let popped = arr.pop(); 
+	let shifted = arr.shift() 
+	return [shifted, popped];
+ }
+ 
+ console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+ 
