0c4fe58de021a22ec6435b9ad10c150653f8ad3f
Anton689 <zizugum@gmail.com>
2021-11-09 17:36:20 +0300

    Basic Data Structures

---
+++ Basic Data Structures.js
@@ -0,0 +1,14 @@
+// Use an Array to Store a Collection of Data
+
+let yourArray; // Change this line
+yourArray = ['one' , 3 ,  false, 'js', 9];
+
+
+//Access an Array's Contents Using Bracket Notation
+
+let myArray = ["a", "b", "c", "d"];
+
+myArray[1] = 'Hello';
+
+console.log(myArray);
+
