5de580a75f4c12a52b590b92737570a349939b5f
Anton689 <zizugum@gmail.com>
2021-11-11 13:45:05 +0300

    changes

--- Basic_topic_17-21.js
+++ Basic_topic_17-21.js
@@ -235,21 +235,6 @@ function nextInLine(arr, item) {
 	return 'No, that was false'; 
 
  }
- // Introducing Else If Statements
-
- function testElseIf(val) {
-	if (val > 10) {
-	  return "Greater than 10";
-	}
- 
-	else if (val < 5) {
-	  return "Smaller than 5";
-	}else{
- 
-	return "Between 5 and 10";
- }
- }
- testElseIf(7);
  // Comparison with the Equality Operator
 
 function testEqual(val) {
