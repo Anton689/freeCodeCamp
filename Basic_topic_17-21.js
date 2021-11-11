5f315d15eed147a00571a1d7b33688e5ebde2447
Anton689 <zizugum@gmail.com>
2021-11-11 12:56:08 +0300

    Comparisons with the Logical Or Operator, Introducing Else Statements

--- Basic_topic_17-21.js
+++ Basic_topic_17-21.js
@@ -357,3 +357,26 @@ function compareEquality(a, b) {
 	return "No";
  }
  testLogicalAnd(10);
+
+ //Comparisons with the Logical Or Operator
+
+ function testLogicalOr(val) {
+	if (val < 10 || val > 20) {
+	  return "Outside";
+	}
+	return "Inside";
+ }
+ testLogicalOr(15);
+
+ //Introducing Else Statements
+
+ function testElse(val) {
+	let result = "";
+	if (val > 5) {
+	  result = "Bigger than 5";
+	}else{
+	  result = "5 or Smaller";
+	}
+	return result;
+ }
+  testElse(4);
