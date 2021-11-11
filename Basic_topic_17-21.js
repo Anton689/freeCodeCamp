9a5fa26c310b5daf5a92e3b467e2137393fb2c65
Anton689 <zizugum@gmail.com>
2021-11-11 12:35:39 +0300

    Comparison with the Greater Than Or Equal To Operator, Comparison with the Less Than Operator, Comparison with the Less Than Or Equal To Operator

--- Basic_topic_17-21.js
+++ Basic_topic_17-21.js
@@ -302,3 +302,48 @@ function compareEquality(a, b) {
 	return "10 or Under";
  }
  testGreaterThan(10);
+
+ //Comparison with the Greater Than Or Equal To Operator
+
+ function testGreaterOrEqual(val) {
+	if (val >= 20) {  // Change this line
+	  return "20 or Over";
+	}
+ 
+	if (val >= 10) {  // Change this line
+	  return "10 or Over";
+	}
+ 
+	return "Less than 10";
+ }
+ 
+ testGreaterOrEqual(10);
+
+ //Comparison with the Less Than Operator
+
+ function testLessThan(val) {
+	if (val < 25) { 
+	  return "Under 25";
+	}
+ 
+	if (val < 55) { 
+	  return "Under 55";
+	}
+ 
+	return "55 or Over";
+ }
+ 
+ testLessThan(10);
+
+ //Comparison with the Less Than Or Equal To Operator
+
+ function testLessOrEqual(val) {
+	if (val <= 12) { 
+	  return "Smaller Than or Equal to 12";
+	}
+	if (val <= 24) { 
+	  return "Smaller Than or Equal to 24";
+	}
+	return "More Than 24";
+ }
+ testLessOrEqual(10);
