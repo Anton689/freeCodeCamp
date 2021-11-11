9b863a3a795c8fbd69e9ada036719e164ae309fe
Anton689 <zizugum@gmail.com>
2021-11-11 14:19:03 +0300

    Compare Scopes of the var and let Keywords, Mutate an Array Declared with const

--- Basic_topic_17-21.js
+++ Basic_topic_17-21.js
@@ -380,3 +380,26 @@ function compareEquality(a, b) {
 	return result;
  }
   testElse(4);
+
+  //Compare Scopes of the var and let Keywords
+
+  function checkScope() {
+	let i = 'function scope';
+	if (true) {
+	  let i = "block scope";
+	  console.log('Block scope i is: ', i);
+	}
+	console.log('Function scope i is: ', i);
+	return i;
+ }
+
+ //Mutate an Array Declared with const
+
+ const s = [5, 7, 2];
+function editInPlace() {
+s[0] = 2;
+s[1] = 5;
+s[2] = 7;
+
+}
+editInPlace();
