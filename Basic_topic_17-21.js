5d27c19d7ea905dcae386a1926c555490eb4c682
Anton689 <zizugum@gmail.com>
2021-11-11 12:43:14 +0300

    Comparisons with the Logical And Operator

--- Basic_topic_17-21.js
+++ Basic_topic_17-21.js
@@ -347,3 +347,13 @@ function compareEquality(a, b) {
 	return "More Than 24";
  }
  testLessOrEqual(10);
+
+ //Comparisons with the Logical And Operator
+
+ function testLogicalAnd(val) {
+	if (val <= 50 && val >= 25) {
+		 return "Yes"; 
+	}
+	return "No";
+ }
+ testLogicalAnd(10);
