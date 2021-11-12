460bf15883b4aa9941857c6fdfd8a287592ee150
Anton689 <zizugum@gmail.com>
2021-11-12 14:48:41 +0300

    Match Literal Strings

--- Basic_topic_17-21.js
+++ Basic_topic_17-21.js
@@ -500,3 +500,9 @@ const arr = removeFirstTwo(source);
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString); // Change this line
+
+//Match Literal Strings
+
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
