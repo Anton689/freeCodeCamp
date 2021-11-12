a9b2c05106ce1706858b8643e433610e21a98305
Anton689 <zizugum@gmail.com>
2021-11-12 14:44:45 +0300

    Using the Test Method

--- Basic_topic_17-21.js
+++ Basic_topic_17-21.js
@@ -494,3 +494,9 @@ function removeFirstTwo(list) {
   return arr;
 }
 const arr = removeFirstTwo(source);
+
+//Using the Test Method
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
