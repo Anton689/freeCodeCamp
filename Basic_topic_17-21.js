3be3d693dffa414df1d93a96692dc151a5c9c7b5
Anton689 <zizugum@gmail.com>
2021-11-13 16:43:00 +0300

    Selecting from Many Options with Switch Statements

--- Basic_topic_17-21.js
+++ Basic_topic_17-21.js
@@ -591,3 +591,29 @@ function golfScore(par, strokes) {
     return names[6];
   }
 }
+
+
+//Selecting from Many Options with Switch Statements
+
+ {
+	let answer = "";
+ switch(val) {
+	case 1:
+	answer = 'alpha'
+	break;
+ 
+	case 2:
+	answer = 'beta'
+	break;
+ 
+	case 3:
+	answer = 'gamma'
+	break; 
+ 
+	case 4:
+	answer = 'delta';
+	break; 
+ }
+	return answer;
+ }
+ 
