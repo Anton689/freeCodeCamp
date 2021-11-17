8bcff183c79d9d772436ba0b0e8f87fe95ec27d0
Anton689 <zizugum@gmail.com>
2021-11-17 14:21:19 +0300

    Add New Properties to a JavaScript Object

--- Basic_topic_17-21.js
+++ Basic_topic_17-21.js
@@ -834,5 +834,16 @@ const testObj = {
  };
  
  myDog.name = 'Happy Coder';
+
+ //Add New Properties to a JavaScript Object
+
+ const myDog = {
+	"name": "Happy Coder",
+	"legs": 4,
+	"tails": 1,
+	"friends": ["freeCodeCamp Campers"]
+ };
+ 
+ myDog.bark = "woof";
  
 
