5ad228f07f3df2669e645f11d707d91187db69ca
Anton689 <zizugum@gmail.com>
2021-11-17 15:01:54 +0300

    Accessing Nested Objects

--- Basic_topic_17-21.js
+++ Basic_topic_17-21.js
@@ -912,5 +912,21 @@ function phoneticLookup(val) {
  }
 	
  ];
+
+ //Accessing Nested Objects
+
+ const myStorage = {
+	"car": {
+	  "inside": {
+		 "glove box": "maps",
+		 "passenger seat": "crumbs"
+		},
+	  "outside": {
+		 "trunk": "jack"
+	  }
+	}
+ };
+ 
+ const gloveBoxContents = myStorage.car.inside['glove box'];
  
 
