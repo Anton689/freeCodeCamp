e8033bd9ad9dd7a10889a8233dee42d8638504cf
Anton689 <zizugum@gmail.com>
2021-11-17 19:45:41 +0300

    Match Letters of the Alphabet

--- Basic_topic_17-21.js
+++ Basic_topic_17-21.js
@@ -1235,6 +1235,12 @@ function sum(arr, n) {
     <!-- add your code above -->
   </body>
 </html>
+
+//Match Letters of the Alphabet
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
 	
 
 
