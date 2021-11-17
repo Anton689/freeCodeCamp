2a9a837622999cf6539792130ad951cbca5ba5b4
Anton689 <zizugum@gmail.com>
2021-11-17 15:36:09 +0300

    Nesting For Loops

--- Basic_topic_17-21.js
+++ Basic_topic_17-21.js
@@ -1023,6 +1023,18 @@ for (let i = 0; i < myArr.length; i ++){
     total += myArr[i];
 }
 
+//Nesting For Loops
+
+function multiplyAll(arr) {
+	let product = 1;
+	for(let i = 0; i < arr.length; i++){
+	  for(let j = 0; j < arr[i].length; j++){
+		 product *= arr[i][j];
+	  }
+	}
+	return product;
+ }
+
 
 
 
