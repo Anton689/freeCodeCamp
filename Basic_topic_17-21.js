45dde780464f67556166bfe85f7b10ab2bc88040
Anton689 <zizugum@gmail.com>
2021-11-17 15:02:45 +0300

    Accessing Nested Arrays

--- Basic_topic_17-21.js
+++ Basic_topic_17-21.js
@@ -928,5 +928,20 @@ function phoneticLookup(val) {
  };
  
  const gloveBoxContents = myStorage.car.inside['glove box'];
+
+ //Accessing Nested Arrays
+
+ var myPlants = [
+	{
+	  type: "flowers",
+	  list: ["rose", "tulip", "dandelion"]
+	},
+	{
+	  type: "trees",
+	  list: ["fir", "pine", "birch"]
+	}
+ ];
+ 
+ var secondTree = myPlants[1].list[1];
  
 
