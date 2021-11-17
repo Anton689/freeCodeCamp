e3e4b30a1f5761002cde33b6b7040c8da05334de
Anton689 <zizugum@gmail.com>
2021-11-17 16:43:10 +0300

    Use Multiple Conditional (Ternary) Operators

--- Basic_topic_17-21.js
+++ Basic_topic_17-21.js
@@ -1124,6 +1124,16 @@ function sum(arr, n) {
 	 
 	 checkEqual(1, 2);
 
+	 //Use Multiple Conditional (Ternary) Operators
+
+	 function checkSign(num) {
+		return (num > 0) ? 'positive'
+		: (num < 0) ? 'negative'
+		: 'zero';
+	 }
+	 
+	 checkSign(10);
+
 
 
 
